2956e08e5055593984d9086b6d6d35e3ef2f13ae
kim-Hong-Sung <152822211+gggwgwg@users.noreply.github.com>
2025-10-28 10:42:36 +0900

    Create rdlock.c

---
+++ chap7/prob1/rdlock.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "student.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        exit(1);
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(1);
+    }
+
+    struct flock lock;
+    struct student record;
+
+    printf("조회할 학번 입력: ");
+    int id; scanf("%d", &id);
+
+    while (read(fd, &record, RECORD_SIZE) > 0) {
+        if (record.id == id) {
+            lock.l_type = F_RDLCK;
+            lock.l_whence = SEEK_CUR;
+            lock.l_start = -RECORD_SIZE;
+            lock.l_len = RECORD_SIZE;
+            fcntl(fd, F_SETLKW, &lock);
+
+            printf("학번:%d 이름:%s 점수:%d\n", record.id, record.name, record.score);
+            sleep(5);
+
+            lock.l_type = F_UNLCK;
+            fcntl(fd, F_SETLK, &lock);
+            break;
+        }
+    }
+
+    close(fd);
+    return 0;
+}
